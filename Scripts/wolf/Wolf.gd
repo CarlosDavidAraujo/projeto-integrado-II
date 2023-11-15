e41ef3b8b2a713303503a26270e9646c54c2c776
CarlosDavidAraujo <cdavidav19@gmail.com>
2023-11-15 10:16:45 -0300

    nova IA do lobo

--- Scripts/wolf/Wolf.gd
+++ Scripts/wolf/Wolf.gd
@@ -2,63 +2,41 @@ extends CharacterBody3D
 class_name Wolf
 signal healthChanged
 
-@export var player: Player
-@export var maxHealth = 75
-@export var damage = 7.5
-@export var fall_acceleration = 75
-@onready var currentHealth: int = maxHealth
-@onready var wolf_anim = $Pivot/AnimationPlayer
-@onready var attack_collision = $AttackHitBox/CollisionShape3D
-@onready var state_machine: StateMachine = $StateMachine
-@onready var damege = $damege
-@onready var evasion_cd : Timer = $EvasionCD
-@onready var attack_cd : Timer = $AttackCD
-@onready var animation_player: AnimationPlayer = $Pivot/AnimationPlayer
-@onready var dash_window: Timer = $DashWindow
+const SPEED = 20
 
-var speed = 20
-var isDead = false
-var is_in_melee_range = false
-var can_evade = true
-var can_attack = true
+var target = null
+@export var targetPath : NodePath
+@export var attack_range: float
 
 func _ready():
-	attack_collision.disabled = true
+	target = get_node(targetPath)
+	healthChanged.emit()
 
-func _physics_process(delta):
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		velocity.y = velocity.y - fall_acceleration
+@export var maxHealth = 30
+@onready var currentHealth: int = maxHealth
+@onready var wolf_anim = $Pivot/AnimationPlayer
+@onready var nav_agent = $NavigationAgent3D
+
+var isDead = false
+
+func _chase(delta):
+	velocity = Vector3.ZERO
+	nav_agent.set_target_position(target.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	look_at(Vector3(target.global_position.x,target.global_position.y,target.global_position.z), Vector3.UP)
 	move_and_slide()
 
+func _is_target_in_range():
+	return global_position.distance_to(target.global_position) < attack_range
+
+func _is_moving():
+	return !(velocity.x == 0 && velocity.z == 0)
+
 func hurt():
-	currentHealth -= player.damage
-	damege.play()
+	currentHealth -= 3
 	if currentHealth <= 0:
 		isDead = true
-		state_machine.set_state("dead")
 	healthChanged.emit()
-	
-func _on_attack_range_body_entered(body):
-	if body.is_in_group("player"):
-		is_in_melee_range = true
-	
-func _on_attack_range_body_exited(body):
-	if body.is_in_group("player"):
-		is_in_melee_range = false
-
-func _on_evasion_cd_timeout():
-	can_evade = true
-	
-func _on_attack_cd_timeout():
-	can_attack = true
-	
-func _on_dash_window_timeout():
-	if not isDead:
-		state_machine.set_state("dash")
-		
-
-
-
-
 
 
---
+++ Scripts/wolf/WolfFSM.gd
@@ -0,0 +1,47 @@
+extends "res://Scripts/_StateMachine.gd"
+
+func _ready():
+	add_state("idle")
+	add_state("run")
+	add_state("dodge")
+	add_state("hit")
+	add_state("dead")
+	call_deferred("set_state", states.idle)
+
+func _state_logic(delta):
+	if [states.run, states.idle].has(state):
+		parent._chase(delta)
+
+func _get_transition(delta):
+	match state:
+		states.idle:
+			if(parent._is_target_in_range()):
+				return states.hit
+			if(parent._is_moving()):
+				return states.run
+		states.run:
+			if(parent._is_target_in_range()):
+				return states.hit
+			if(!parent._is_moving()):
+				return states.idle
+	return null
+
+func _enter_state(new_state, old_state):
+	match new_state:
+		states.idle:
+			parent.wolf_anim.play("idle")
+		states.run:
+			parent.wolf_anim.play("run")
+		states.hit:
+			parent.wolf_anim.play("dash")
+		states.dodge:
+			parent.wolf_anim.play_backwards("pulo")
+		states.dead:
+			parent.wolf_anim.play("idle")			
+
+func _exit_state(old_state, new_state):
+	pass
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "dash":
+		set_state(states.idle)
