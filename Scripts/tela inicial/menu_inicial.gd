5f544ba2416ad24e0a1ad52490dcb83f3eb15aba
mangetsuZ <bielmn2@gmail.com>
2023-11-15 16:13:28 -0300

    menu inicial
    implementação do menu inicial, falta alterar para o main abrir corretamente na cena do menu ao invés do level 1

---
+++ Scripts/tela inicial/menu_inicial.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_jogar_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+
+
+func _on_controles_pressed():
+	pass # Replace with function body.
+
+
+func _on_sair_pressed():
+	get_tree().quit()
