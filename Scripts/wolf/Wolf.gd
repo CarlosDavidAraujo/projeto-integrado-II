2f42d197650905b1f5f95fd5096ba983427d2776
Victor Furtado <victor.furtado@coden3.com.br>
2023-11-16 12:11:10 -0300

    new wolf ai

--- Scripts/wolf/Wolf.gd
+++ Scripts/wolf/Wolf.gd
@@ -2,11 +2,12 @@ extends CharacterBody3D
 class_name Wolf
 signal healthChanged
 
-const SPEED = 50
+const SPEED = 20
 
 var target = null
 @export var targetPath : NodePath
 @export var attack_range: float
+@export var too_far: float
 @export var player: Player
 
 func _ready():
@@ -17,27 +18,54 @@ func _ready():
 @onready var currentHealth: int = maxHealth
 @onready var wolf_anim = $Pivot/AnimationPlayer
 @onready var nav_agent = $NavigationAgent3D
-@onready var damege = $damege
+@onready var damageSFX = $damageSFX
+@onready var cd = $StateMachine/cd
 
+var is_cd_able = true
+
+var init = false
 var isDead = false
 
+func start():
+	init = true
+
 func _chase(delta):
+	__move(delta,1)
+
+func _dodge(delta):
+	__move(delta,-1)
+
+func _dash(delta):
+	__move(delta,10)
+
+func __move(delta, mod):
 	velocity = Vector3.ZERO
 	nav_agent.set_target_position(target.global_transform.origin)
 	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED*mod
 	look_at(Vector3(target.global_position.x,global_position.y,target.global_position.z), Vector3.UP)
 	move_and_slide()
 
 func _is_target_in_range():
 	return global_position.distance_to(target.global_position) < attack_range
 
+
+func _is_target_too_far():
+	return global_position.distance_to(target.global_position) > too_far
+
+func _cd_start(time):
+	cd.wait_time = time
+	cd.start()
+
+func _cd_stop():
+	cd.stop()
+
 func _is_moving():
 	return !(velocity.x == 0 && velocity.z == 0)
 
 func hurt():
 	if currentHealth > 0:
-		damege.play()
+		damageSFX.play()
 		currentHealth -= player.damage
 	else:
 		currentHealth = 0
@@ -45,3 +73,7 @@ func hurt():
 	healthChanged.emit()
 
 
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player"):
+		if body.has_method("hurt"):
+			body.hurt(20)
--- Scripts/wolf/WolfFSM.gd
+++ Scripts/wolf/WolfFSM.gd
@@ -1,34 +1,56 @@
 extends "res://Scripts/_StateMachine.gd"
 
+@onready var debug = $SubViewport/Label
+
 func _ready():
 	add_state("idle")
 	add_state("run")
 	add_state("dodge")
 	add_state("hit")
+	add_state("dash")
 	add_state("dead")
 	call_deferred("set_state", states.idle)
 
 func _state_logic(delta):
-	if [states.run, states.idle].has(state):
+	debug.text = "State: {0}".format(state)
+	
+	if [states.run].has(state):
 		parent._chase(delta)
+	if [states.dash].has(state):
+		parent._dash(delta)
+	if [states.dodge].has(state):
+		parent._dodge(delta)
+		
+	if parent._is_target_too_far():
+		print("Too far")
+		if parent.cd.is_stopped():
+			parent._cd_start(5)
+	else:
+		print("Close enough")
+		parent._cd_stop()
 
 func _get_transition(delta):
-	if parent.currentHealth <= 0:
+	if !parent.init:
+		return null
+	if parent.isDead:
 		return states.dead
 	match state:
 		states.idle:
 			if(parent._is_target_in_range()):
 				return states.hit
+			else:
+				return states.run
 			if(parent._is_moving()):
 				return states.run
 		states.run:
 			if(parent._is_target_in_range()):
 				return states.hit
-			if(!parent._is_moving()):
+			elif(!parent._is_moving()):
 				return states.idle
 	return null
 
 func _enter_state(new_state, old_state):
+	parent.wolf_anim.play("RESET")
 	match new_state:
 		states.idle:
 			parent.wolf_anim.play("idle")
@@ -36,14 +58,24 @@ func _enter_state(new_state, old_state):
 			parent.wolf_anim.play("run")
 		states.hit:
 			parent.wolf_anim.play("dash")
+		states.dash:
+			parent.wolf_anim.play("dash")
 		states.dodge:
-			parent.wolf_anim.play_backwards("pulo")
+			parent.wolf_anim.play("pulo")
 		states.dead:
-			parent.wolf_anim.play("idle")			
+			parent.wolf_anim.play("idle")
 
 func _exit_state(old_state, new_state):
 	pass
 
 func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "dash":
+	if ["dash"].has(anim_name):
+		set_state(states.dodge)
+	if ["pulo"].has(anim_name):
 		set_state(states.idle)
+
+func _on_cd_timeout():
+	print("ATTACK")
+	if parent._is_target_too_far():
+		print("NOW!")
+		set_state("dash")
